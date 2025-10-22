2af476fe4e07d3ff17a67873fefc5cd6c15359be
cry4death <maxmaxret@gmail.com>
2025-10-22 12:55:20 +0300

    update

---
+++ src/main/java/org/example/servlets/MainServlet.java
@@ -0,0 +1,63 @@
+package org.example.servlets;
+
+
+import org.example.servlets.entity.StudentService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.stream.Collectors;
+
+
+public class MainServlet extends HttpServlet {
+
+    private StudentService studentService;
+
+    public MainServlet() {
+        this.studentService = new StudentService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // testig the get methods before implementation
+        String jsonResponse = this.studentService.findAllStudents();
+        this.outputResponse(resp, jsonResponse, 200);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // add student from post body
+        String reqBody = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+
+        int rc = HttpServletResponse.SC_OK;
+        boolean res = this.studentService.createStudent(reqBody);
+        if (!res) {
+            rc = HttpServletResponse.SC_BAD_REQUEST;
+        }
+        this.outputResponse(resp, null, rc);
+    }
+
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////// private methods ////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private void outputResponse(HttpServletResponse response, String payload, int status) {
+        response.setHeader("Content-Type", "application/json");
+        try {
+            response.setStatus(status);
+            if (payload != null) {
+                OutputStream outputStream = response.getOutputStream();
+                outputStream.write(payload.getBytes());
+                outputStream.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
